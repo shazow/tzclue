7903ffc63fbc9c171c0dbc1621be7f047d89b3f6
Andrey Petrov <andrey.petrov@shazow.net>
2018-07-22 14:21:55 -0400

    wip: Properties.Set works with dbus.MakeVariant

--- main.go
+++ main.go
@@ -30,34 +30,15 @@ func main() {
 
 	// Setup client
 
-	// Set DesktopID
-	if call := client.Call("org.freedesktop.DBus.Properties.Set", 0, "org.freedesktop.GeoClue2.Client.DesktopId", appName); call.Err != nil {
-		/*
-			This errors with:
-				call failed: Properties.Set: No such interface 'org.freedesktop.DBus.Properties' on object at path /org/freedesktop/GeoClue2/Client/1
-
-			And yet,
-			$ qdbus -r --system org.freedesktop.GeoClue2 /org/freedesktop/GeoClue2/Client/1
-			method void org.freedesktop.DBus.Properties.Set(QString interface_name, QString property_name, QDBusVariant value)
-			...
-			property readwrite QString org.freedesktop.GeoClue2.Client.DesktopId
-			$ qdbus --system org.freedesktop.GeoClue2 /org/freedesktop/GeoClue2/Client/1 "org.freedesktop.DBus.Properties.Set" org.freedesktop.GeoClue2.Client DesktopId "foo"
-			Error: org.freedesktop.DBus.Error.AccessDenied
-		*/
+	// Set DesktopId
+	if call := client.Call("org.freedesktop.DBus.Properties.Set", 0, "org.freedesktop.GeoClue2.Client", "DesktopId", dbus.MakeVariant(appName)); call.Err != nil {
 		exit(2, "call failed: Properties.Set: %s\n", call.Err)
 	}
 
-	// Set DistanceThreshold
-	//client.Call("org.freedesktop.DBus.Properties.Set", 0, "org.freedesktop.GeoClue2.Client.DistanceThreshold", 0)
-	/*
-		ret_v = g_dbus_proxy_call_sync(
-		geoclue_client,
-		"org.freedesktop.DBus.Properties.Set",
-		g_variant_new("(ssv)",
-		"org.freedesktop.GeoClue2.Client",
-		"DistanceThreshold",
-		g_variant_new("u", 50000)),
-	*/
+	// Set DesktopId
+	//if call := client.Call("org.freedesktop.DBus.Properties.Set", 0, "org.freedesktop.GeoClue2.Client", "DistanceThreshold", dbus.MakeVariant(50000)); call.Err != nil {
+	//	exit(2, "call failed: Properties.Set: %s\n", call.Err)
+	//}
 
 	// Subscribe to signal
 	/*
@@ -70,7 +51,6 @@ func main() {
 	if call := conn.BusObject().Call("org.freedesktop.DBus.AddMatch", 0,
 		"type='signal',path='/org/freedesktop/DBus',interface='org.freedesktop.DBus',sender='org.freedesktop.DBus'"); call.Err != nil {
 		exit(2, "call failed: AddMatch: %s", call.Err)
-
 	}
 
 	sig := make(chan *dbus.Signal, 10)
